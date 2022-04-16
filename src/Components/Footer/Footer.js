7675e6745ad0167c4f4eea707599238c74b74863
toufiqul-hasan <toufiqul.shihab@gmail.com>
2022-04-17 02:34:44 +0600

    footer added

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p className='mt-3'>&copy; Dental Plus 2022 All Rights Reserved.</p>
+    </div>
+  );
+};
+
+export default Footer;
