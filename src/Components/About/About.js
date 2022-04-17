f747086a6a15fb6b74d3cc8d9704026a4271376b
toufiqul-hasan <toufiqul.shihab@gmail.com>
2022-04-17 09:42:37 +0600

    about added

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React from "react";
+import me from "../../Assets/Images/me.png";
+import "./About.css";
 
 const About = () => {
   return (
     <div>
-      
+      <img src={me} alt="" />
+      <p>
+        I want to become a web developer in 2022. My goal is become a full stack
+        web developer in 2022. I consider myself a diligent individual. To
+        succeed in an environment of growth and excellence and earn a job which
+        provides me job satisfaction, self-development, having career
+        advancement, and help me achieve personal as well as organizational
+        goals.
+      </p>
     </div>
   );
 };
